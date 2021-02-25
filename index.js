7eb9ca7f6882ce6811182aaaa539cc0e7d4a2d59
e-licea <48616990+e-licea@users.noreply.github.com>
2021-02-25 03:33:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server');
 
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 server.listen(PORT, ()=>{
     console.log(`=== Server is listening on PORT ${PORT} ===`)
