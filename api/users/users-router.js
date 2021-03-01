3017a6bf49f7a8ae91d4765306597ebdfd49721e
William Jensen <williambjensen01@gmail.com>
2021-03-01 14:49:41 -0700

    working through crud operations for user

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -4,27 +4,34 @@ const router = express.Router();
 
 const Items = require('./users-model');
 
-// GET /api/users/:id
-router.get('/:id', restricted, async (req, res) => {
+// GET /api/users/:user/items
+router.get('/:user/items', restricted, async (req, res) => {
     try {
-        const userItems = await Items.get(req.params.id);
+        const userItems = await Items.get(req.params.user);
         res.status(200).json(userItems);
     } catch (err) {
-        res.status(500).json({ message: "Server failed to get user items", error: err })
+        res.status(500).json({ message: "Server failed to get user items", error: err });
     }
 });
 
-// router.post('/', restricted, async (req, res) => {
-//     const body = req.body;
-//     if (!body.name || !body.price) {
-//         res.status(400).json({ message: "Body must include name and price." })
-//     }
-//     try {
-//         const newItem = await Items.add(req.body);
-//         res.status(201).json(newItem)
-//     } catch (err) {
-//         res.status(500).json({ message: "Server failed to add new item." })
-//     }
-// });
+// GET /api/users/:user/items/:id
+router.get('/:user/items/:id', restricted, async (req, res) => {
+    try {
+        const userItem = await Items.getById(req.params.user, req.params.id);
+        res.status(200).json(userItem);
+    } catch (err) {
+        res.status(500).json({ message: "Server failed to get user item", error: err });
+    }
+});
+
+// PUT /api/users/:user/items/:id
+router.put('/:user/items/:id', restricted, async (req, res) => {
+    try {
+        const updatedItem = await Items.update(req.params.user, req.params.id, req.body);
+        res.status(200).json(updatedItem);
+    } catch (err) {
+        res.status(500).json({ message: "Server failed to update item", error: err })
+    }
+});
 
 module.exports = router
