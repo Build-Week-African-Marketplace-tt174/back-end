b6ba81e17064f6c26791dac988c46e5f6f852e17
William Jensen <williambjensen01@gmail.com>
2021-03-01 12:15:49 -0700

    created user_items table that references users id and items id

--- data/migrations/20210224231317_marketplace.js
+++ data/migrations/20210224231317_marketplace.js
@@ -19,8 +19,13 @@ exports.up = async function(knex) {
       tbl.string('location');
       tbl.string('photo_url');
     })
+    .createTable('user_items', tbl => {
+      tbl.integer('user_id').unsigned().notNullable().references('users.id').onDelete('CASCADE').onUpdate('CASCADE');
+      tbl.integer('item_id').unsigned().notNullable().references('items_id').onDelete('CASCADE').onUpdate('CASCADE');
+      tbl.primary(['user_id', 'item_id']);
+    })
 };
 
 exports.down = async function(knex) {
-    return await knex.schema.dropTableIfExists('users').dropTableIfExists('items');
+    return await knex.schema.dropTableIfExists('user_items').dropTableIfExists('items').dropTableIfExists('users');
 };
