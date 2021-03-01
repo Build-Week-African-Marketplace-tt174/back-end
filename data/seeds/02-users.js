4fda9b31bddab0b272f9a78ec72512f8c609f596
William Jensen <williambjensen01@gmail.com>
2021-03-01 13:51:24 -0700

    created a new user

--- data/seeds/02-users.js
+++ data/seeds/02-users.js
@@ -4,6 +4,7 @@ exports.seed = function(knex) {
   const hash = bcrypt.hashSync('password', 10);
 
   return knex('users').insert([
-    {company: 'The Electric Company', password: hash, username: 'testuser1', email: 'testuser@email.com'},
+    { company: 'The Electric Company', password: hash, username: 'testuser1', email: 'testuser@email.com' },
+    { company: 'Dunder Mifflin', password: hash, username: 'michaelscarn', email: 'dundermifflinpaper@gmail.com' }
   ]);
 };
